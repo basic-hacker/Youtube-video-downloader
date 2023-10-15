f93c6dfa20fff2cf11cb5b126bf28df5a62d33e5
Smael <147974069+basic-hacker@users.noreply.github.com>
2023-10-15 23:20:10 +0530

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -2,23 +2,18 @@ from pytube import YouTube
 import shutil
 import os
 
-
-# Check if the required libraries are installed, if not, install them
 try:
     import pytube
 except ImportError:
     print("pytube not installed. Installing...")
     os.system("pip install -r requirements.txt")
 
-# Rest of your code...
 def clear_screen():
     # Clear the screen based on ANSI escape codes
     os.system('cls' if os.name == 'nt' else 'clear')
 
-# Clear the screen
 clear_screen()
 
-# The ASCII art banner
 banner = """
 :'######::'##::::'##::::'###::::'########:'##:::::::
 '##... ##: ###::'###:::'## ##::: ##.....:: ##:::::::
@@ -29,22 +24,15 @@ banner = """
 . ######:: ##::::: ##: ##:::: ##: ########: ########:
 :......:::..:::::::..::..:::::..::........::........::
 """
-
-# Get the terminal width
 terminal_width = shutil.get_terminal_size().columns
 
-# Calculate the number of spaces needed to center the banner
 num_spaces = (terminal_width - max(len(line) for line in banner.split('\n'))) // 2
 
-# Print the banner with centered alignment
 print('\n'.join(' ' * num_spaces + line for line in banner.split('\n')))
 
-
-# Prompt the user to input the YouTube video URL
 video_url = input("Enter the YouTube video URL: ")
 
 try:
-    # Create a YouTube object
     yt = YouTube(video_url)
 
     print("Options:")
@@ -54,13 +42,11 @@ try:
     choice = int(input("Enter your choice (1 or 2): "))
 
     if choice == 1:
-        # Display available video streams and resolutions
         video_streams = yt.streams.filter(file_extension='mp4')
         print("Available video resolutions:")
         for i, stream in enumerate(video_streams, start=1):
             print(f"{i}. {stream.resolution}")
 
-        # Prompt the user to choose a resolution
         chosen_number = int(input("Enter the number for the resolution you want to download: "))
 
         if 1 <= chosen_number <= len(video_streams):
@@ -72,11 +58,9 @@ try:
             print('Invalid number. Please choose a valid number.')
 
     elif choice == 2:
-        # Get the highest quality audio stream
         audio_stream = yt.streams.filter(only_audio=True).first()
 
         if audio_stream:
-            # Download the audio as MP3
             audio_stream.download(filename_prefix='audio_')
             print('Download completed! (MP3)')
         else:
